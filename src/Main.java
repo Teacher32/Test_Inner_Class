f2187955785edd1489f859b1b820e3c420309ac8
Teacher32 <kitaev-yuri@yandex.ru>
2021-01-31 14:54:29 +0300

    Добавлены комментарии в класс Мэйн

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-public class Main {
-    public static void main(String[] args) {
-        TVSet tv = new TVSet();
-        System.out.println(tv.toString());
+public class Main {                             // класс "Мэйн"
+    public static void main(String[] args) {        // метод "мэйн"
+        TVSet tv = new TVSet();                 // создаем объект класса "Телевизор"
+        System.out.println(tv.toString());      // печатаем сообщение в зависимости от переменной isOn
     }
 }
