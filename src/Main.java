9c094ff7cd9544259a99927db9287dcf95d80669
Teacher32 <kitaev-yuri@yandex.ru>
2021-01-31 09:29:01 +0300

    Добавлено тело в метод main.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        TVSet tv = new TVSet();
+        System.out.println(tv.toString());
     }
 }
--- src/TVSet.java
+++ src/TVSet.java
@@ -2,4 +2,12 @@ public class TVSet {            // создаем класс Телевизор
     int channel = 5;            // определяем переменную "Канал"
     boolean isOn = false;       // определяем переменную "Включен?"
 
+    public String toString() {
+        if (!isOn) {
+            return "Телевизор выключен";
+        }
+        else {
+            return "Канал № " + channel + " включен.";
+        }
+    }
 }
