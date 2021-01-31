1380195fc3b91cff1c800eb72a334f9f30de56af
Teacher32 <kitaev-yuri@yandex.ru>
2021-01-31 15:43:54 +0300

    Добавлены Методы в класс Remote

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {                             // класс "Мэйн"
     public static void main(String[] args) {        // метод "мэйн"
         TVSet tv = new TVSet();                 // создаем объект класса "Телевизор"
-        System.out.println(tv.toString());      // печатаем сообщение в зависимости от переменной isOn
+        TVSet.Remote remote = new TVSet.Remote();
+        TVSet.Remote.setTVSet(tv);
+
+        // System.out.println(tv.toString());      // печатаем сообщение в зависимости от переменной isOn
     }
 }
--- src/TVSet.java
+++ src/TVSet.java
@@ -1,13 +1,33 @@
 public class TVSet {            // создаем класс Телевизор
-    int channel = 5;            // определяем переменную "Канал"
+    static int channel = 5;            // определяем переменную "Канал"
     boolean isOn = false;       // определяем переменную "Включен?"
 
-    public String toString() {
+    public String toString() {  // используем метод "туСтринг" класса "Обджект" для того, чтобы иметь возможность выводить строку (иначе будут хэш-символы)
         if (!isOn) {
             return "Телевизор выключен";
         }
         else {
             return "Канал № " + channel + " включен.";
+        }
+    }
+
+    public static class Remote {                  // создаем вложенный класс "Пульт"
+        private TVSet tvSet;        // создаем ссылку объект класс "Телевизор"
+
+        public static void setTVSet(TVSet tvSet) {    // создаем метод, который связывает Телевизор с пультом
+            tvSet = tvSet;     // получаем доступ к объекту tvSet внутри класса "Пульт"
+        }
+
+        public void setChannel() {  // создаем метод, переключающий (задающий) каналы
+            tvSet.channel = channel;    // значение канала берем из класса TVSet
+        }
+
+        public void turnOn() {      // создаем метод, включающий телевизор
+            tvSet.isOn = true;      // меняем значение переменной "Включен?" на "истина"
+        }
+
+        public void turnOff() {     // создаем метод, выключающий телевизор
+            tvSet.isOn = false;     // Меняем значение переменной "Включен?" на "ложь"
         }
     }
 }
